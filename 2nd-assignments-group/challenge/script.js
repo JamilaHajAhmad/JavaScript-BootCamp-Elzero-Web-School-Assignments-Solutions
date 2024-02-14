c13f1a95a05326ba0f7988d251289f38352e25dd
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2024-02-14 08:04:55 +0200

    Second Group Of JS Assignments

---
+++ 2nd-assignments-group/challenge/script.js
@@ -0,0 +1,8 @@
+let theTitle = 'Elzero', theDescription = 'Elzero Web School', theDate = '25/10';
+let card = 
+` <div> 
+<h3> Hello ${theTitle} </h3>
+<p> ${theDescription} </p>
+<span> ${theDate} </span>
+</div> ` ;
+document.write(card.repeat(4));
