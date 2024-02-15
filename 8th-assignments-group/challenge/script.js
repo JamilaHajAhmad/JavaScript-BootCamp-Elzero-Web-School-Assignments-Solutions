36632b43e333010fa03872479c68d50c5835cc0c
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2024-02-16 00:28:46 +0200

    Eighth Group Of JS Assignments

---
+++ 8th-assignments-group/challenge/script.js
@@ -0,0 +1,55 @@
+let job = "Manager";
+let salary = 0;
+
+switch(job) {
+
+    case "Manager":
+        salary = 8000;
+        console.log(salary);
+    break;
+    
+    case "IT":
+    case "Support":
+        salary = 6000;
+        console.log(salary);
+
+        break;
+    
+    case "Developer":
+    case "Designer":
+        salary = 7000;
+        console.log(salary);
+    break;
+
+    default:
+        salary = 4000;
+        console.log(salary);
+}
+
+let holidays = 0;
+let money = 0;
+
+if(holidays === 0) {
+    money = 5000;
+    console.log(`My Money = ${money}`);
+}
+else if(holidays === 1 || holidays === 2) {
+    money = 3000;
+    console.log(`My Money = ${money}`);
+}
+else if(holidays === 3) {
+    money = 2000;
+    console.log(`My Money = ${money}`);
+}
+else if(holidays === 4) {
+    money = 1000;
+    console.log(`My Money = ${money}`);
+}
+else if(holidays === 5) {
+    money = 0;
+    console.log(`My Money = ${money}`);
+}
+else {
+    money = 0;
+    console.log(`My Money = ${money}`);
+}
