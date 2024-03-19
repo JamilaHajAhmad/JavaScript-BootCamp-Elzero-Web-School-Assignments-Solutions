f6bb2f09eca7f6dfc5b41f51885856222759ecc0
Jamila HajAhmad <124258273+JamilaHajAhmad@users.noreply.github.com>
2024-03-19 11:29:31 +0200

    Delete 22th-assignments-group directory

--- 22th-assignments-group/assignment-4/script.js
+++
@@ -1,8 +0,0 @@
-// Write Your Code Here
-String.prototype.addLove = function() {
-    return `I Love ${this} Web School`;
-}
-
-// Do Not Edit Below
-let myStr = "Elzero";
-console.log(myStr.addLove()); // I Love Elzero Web School
